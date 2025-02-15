9c11ff520e9ea890c359e1b681877f2311cb7a31
Lakshman <lakshmansai9978@gmail.com>
2025-02-15 19:46:46 +0530

    Reverse String Words

---
+++ Problem_Solving_with_Java/src/com/dsa/strings/ReverseStringWords.java
@@ -0,0 +1,17 @@
+package com.dsa.strings;
+
+public class ReverseStringWords {
+	 public String reverseWords(String s) {
+	        
+	        String[] str=s.split(" +");
+	        StringBuilder sb=new StringBuilder();
+
+	        for(int i=str.length-1;i>=0;i--)
+	        {
+	           sb.append(str[i]);
+	           sb.append(" ");
+	        }
+	        return sb.toString().trim();
+	        
+	    }
+}
