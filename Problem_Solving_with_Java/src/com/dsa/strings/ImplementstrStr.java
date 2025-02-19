4abc8e3b6fbba654fea9d3f4ec292b9bb0601291
Lakshman <lakshmansai9978@gmail.com>
2025-02-19 10:07:16 +0530

    Added two String Programs

---
+++ Problem_Solving_with_Java/src/com/dsa/strings/ImplementstrStr.java
@@ -0,0 +1,30 @@
+package com.dsa.strings;
+
+/* Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.*/
+
+public class ImplementstrStr {
+	public int strStr(String haystack, String needle) {
+        for(int i=0;i<=haystack.length()-needle.length();i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(0))
+            {
+                if(haystack.substring(i,needle.length()+i).equals(needle))
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
